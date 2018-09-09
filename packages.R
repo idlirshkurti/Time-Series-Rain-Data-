dd5bbcf16ccbe0e7937672a946c4a19811b781b1
Idlir Shkurti <idlir.sh@gmail.com>
2018-09-09 14:03:46 +0200

    Update packages.R

--- packages.R
+++ packages.R
@@ -1,3 +1,4 @@
+# Call this from all the other scripts in order to load libraries
 packages <- c('ggplot2', 'nlme', 'mgcv', 'TSA', 'forecast', 'devtools', 'ggfortify',
  'reshape2', 'stats', 'dlm', 'grid', 'gridExtra','scales')
 lapply(packages, require, character.only = TRUE)
